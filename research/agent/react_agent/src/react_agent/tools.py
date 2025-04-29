8b6f7aaded4477890aa0fb674f8449775f4c8970
ivan_zaytsev <alexzaitsev9@gmail.com>
2025-04-30 02:11:49 +0300

    meow

--- research/agent/react_agent/src/react_agent/tools.py
+++ research/agent/react_agent/src/react_agent/tools.py
@@ -13,6 +13,8 @@ from langchain_tavily import TavilySearch  # type: ignore[import-not-found]
 from react_agent.configuration import Configuration
 from langchain_core.tools import tool
 
+import json
+
 
 # async def search(query: str) -> Optional[dict[str, Any]]:
 #     """Search for general web results.
@@ -28,5 +30,27 @@ from langchain_core.tools import tool
 def search(query:str) -> str:
     """Ищет информацию нужную пользователю"""
     return f"Крутая  информация про {query}"
+
+@tool
+def get_individual_achivements() -> str:
+    """Получает JSON с информацией об индивидуальных достижениях"""
+    with open('research/agent/react_agent/src/achievements.json', 'r') as file:
+        data = json.load(file)
+    return f"JSON с информацией об индивидуальных достижениях \n {data}"
+
+@tool
+def count_individual_achivements(achivements_list:list) -> str:
+    """Подсчитывает сумму баллов за индивидуальные достижения"""
+    with open('research/agent/react_agent/src/achievements.json', 'r') as file:
+        data = json.load(file)
+    total = 0
+    for achievement in achivements_list:
+        total += data.get(achievement, 0)
+    return f"Сумма индивидуальных достижений пользователя: \n {total % 10}"
+
+
+
+
+
 
 TOOLS: List[Callable[..., Any]] = [search]
