0a00f48d00b7f2828df9e3661190b20674578c45
lenikss <anciborko04@mail.ru>
2025-04-30 13:09:37 +0300

    фикс путей

--- src/telegram_bot/app/database.py
+++ src/telegram_bot/app/database.py
@@ -5,14 +5,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
-load_dotenv(override=True)
+import os
+from sqlalchemy import create_engine
 
-DB_URL = os.getenv("DB_URL")
+# Путь к базе данных
+db_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..", ".." "database")
+os.makedirs(db_dir, exist_ok=True)
+db_path = os.path.join(db_dir, "bot.db")
+
+# Создание движка
+engine = create_engine(f"sqlite:///{db_path}", connect_args={"check_same_thread": False})
+
+load_dotenv(override=True)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
 
-
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 Base.metadata.create_all(bind=engine)
