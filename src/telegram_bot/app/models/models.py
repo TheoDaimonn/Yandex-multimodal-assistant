fdc4b26250779914faf4806a8195d1638155ccb7
lenikss <anciborko04@mail.ru>
2025-04-30 13:54:20 +0300

    ебаная база как она меня затрахала уже но модельки создались аминь

--- src/telegram_bot/app/models/models.py
+++ src/telegram_bot/app/models/models.py
@@ -1,9 +1,18 @@
 from sqlalchemy import Column, Integer, Boolean, String, JSON, ForeignKey, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.ext.declarative import declarative_base
-from src.telegram_bot.app.models.User import User
+from sqlalchemy.ext.asyncio import AsyncAttrs
 from src.telegram_bot.app.database import Base
-from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
+    username: Mapped[str | None] = mapped_column(String(255))
+    is_admin: Mapped[bool] = mapped_column(default=False)
+
+    sessions: Mapped[list["Session"]] = relationship("Session", back_populates="users")
 
 
 class Session(Base):
