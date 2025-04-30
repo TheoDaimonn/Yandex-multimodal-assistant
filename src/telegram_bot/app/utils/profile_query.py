044fbd3ffcdf735be608e037964a7a0f46bebc07
Maria <Maria>
2025-04-30 19:50:22 +0300

    clean

--- src/telegram_bot/app/utils/profile_query.py
+++ src/telegram_bot/app/utils/profile_query.py
@@ -6,9 +6,9 @@ from ycloud import YCloudML
 from yandex_cloud_ml_sdk import YCloudML
 
 folder_id = os.environ["FOLDER_ID"]
-api_key   = os.environ["API_KEY"]
-sdk       = YCloudML(folder_id=folder_id, auth=api_key)
-model     = sdk.models.completions("yandexgpt", model_version="rc")
+api_key = os.environ["API_KEY"]
+sdk = YCloudML(folder_id=folder_id, auth=api_key)
+model = sdk.models.completions("yandexgpt", model_version="rc")
 
 def profile_query(dialog: str) -> dict:
     with open('../../data/prompts/profile.md', 'r', encoding="utf-8") as f:
